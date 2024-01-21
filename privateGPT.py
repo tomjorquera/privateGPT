ef78b176d03a31b90060af62c05e71d9e8427179
Tom Jorquera <tom@jorquera.net>
2024-01-21 22:19:49 +0100

    Add option to use GPU

--- privateGPT.py
+++ privateGPT.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
 import os
 import argparse
+from warnings import warn
 
 load_dotenv()
 
@@ -16,7 +17,13 @@ persist_directory = os.environ.get('PERSIST_DIRECTORY')
 model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS', 4))
+use_gpu = os.environ.get("USE_GPU", "").lower() in ("yes", "true", "t", "1")
+llamacpp_n_gpu_layers = os.environ.get('MODEL_N_GPU_LAYERS', None)
+
+# Warn user of options combinations that do not make sense
+if llamacpp_n_gpu_layers is not None and not (use_gpu and model_type == "LlamaCpp"):
+    warn("Setting 'MODEL_N_GPU_LAYERS' has no effect unless 'USE_GPU' == True and 'MODEL_TYPE' == 'LlamaCpp'")
 
 from constants import CHROMA_SETTINGS
 
@@ -31,9 +38,13 @@ def main():
     # Prepare the LLM
     match model_type:
         case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
+            n_gpu_layers = llamacpp_n_gpu_layers if use_gpu else None
+            if use_gpu and n_gpu_layers is None:
+                n_gpu_layers = -1
+            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_gpu_layers=n_gpu_layers, callbacks=callbacks, verbose=False)
         case "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
+            device = "gpu" if use_gpu else "cpu"
+            llm = GPT4All(model=model_path, max_tokens=model_n_ctx, device=device, callbacks=callbacks, verbose=False)
         case _default:
             print(f"Model {model_type} not supported!")
             exit;
